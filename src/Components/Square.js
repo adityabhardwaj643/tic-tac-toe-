eb505c4543c3fdb1f70fe8cecce4f6e3e9f4c835
Aditya Bhardwaj <114653579+adityabhardwaj643@users.noreply.github.com>
2023-06-25 03:02:08 +0530

    basic

---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square(props) {
+  return (
+    <div className="square" onClick={props.onClick} style={{ border: '1px solid', height: '100px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h5>{props.value}</h5>
+    </div>
+  );
+}
+
+export default Square;
